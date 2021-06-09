2dac4ced5795cc3a91f5d8934a12df85a4b5a4a5
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-06-09 13:02:01 +0200

    SPA-components-state-lotto-solution

---
+++ SPA/exercises-solutions/SPA-components-state-lotto-solution/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LuckyNumbers from './components/lucky-numbers';
+
+function App() {
+  return (
+    <div className="App">
+      <LuckyNumbers />
+    </div>
+  );
+}
+
+export default App;
