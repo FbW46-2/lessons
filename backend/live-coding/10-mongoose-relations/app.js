16662296a134b8127e60c32b992398473f2faa86
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2021-08-16 15:17:16 +0200

    populate console log

--- backend/live-coding/10-mongoose-relations/app.js
+++ backend/live-coding/10-mongoose-relations/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const util = require("util");
+
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/relations", {
@@ -26,34 +28,26 @@ const commentSchema = new Schema({
 const postSchema = new Schema({
   text: String,
   username: String,
+  //referencing: comments array contains references to (IDs) comments, not actual comments
   comments: [{ type: Schema.Types.ObjectId, ref: "comment" }],
 });
 
 const PostModel = mongoose.model("post", postSchema);
 const CommentModel = mongoose.model("comment", commentSchema);
 
-PostModel.find({})
-  .populate("comment")
-  .exec((err, post) => console.log(post));
-
 // addPost
-// const aPost = new PostModel({ text: "Hello MongoDB!", username: "Bob" });
-// //aPost.comments.push({ text: "Great post!", username: "Tom" });
+// const aPost = new PostModel({ text: "Second MongoDB!", username: "Bob" });
 
-// const aComment = new CommentModel({ text: "Good post!", username: "Jon" });
+// // addComment
+// const aComment = new CommentModel({ text: "Great post!", username: "Tom" });
+
 // aComment.save();
 
 // aPost.comments.push(aComment);
 // aPost.save();
-
-// aPost
-//   .save()
-//   .then(() => {
-//     console.log("Post saved!");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+PostModel.find({})
+  .populate("comments")
+  .exec((err, post) => console.log(util.inspect(post, true, 10)));
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
