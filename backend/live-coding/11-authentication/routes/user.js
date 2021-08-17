da01f1bce3be1d05244c24a2f4f9267e65fb4fe6
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2021-08-17 14:47:29 +0200

    signup done

--- backend/live-coding/11-authentication/routes/user.js
+++ backend/live-coding/11-authentication/routes/user.js
@@ -30,8 +30,35 @@ router.post(
       if (user) {
         res.status(400).json({ msg: "User Already Exists" });
       }
+
+      //Create a new user
+      user = new User({
+        username,
+        email,
+        password, //"password": "1234"
+      });
+
+      //hash the password for the new user in order to save to DB
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await User.save();
+
+      //Create a payload with user ID in order to create JWT
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, "randomString", { expiresIn: "1h" }, (err, token) => {
+        if (err) throw err;
+        res.status(200).json({ token });
+      });
     } catch (error) {
       res.status(500).send("Error in Saving!");
     }
   }
 );
+
+module.exports = router;
