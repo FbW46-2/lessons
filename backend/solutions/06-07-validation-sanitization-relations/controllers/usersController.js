7cdaaa801172a97289ebdb942ea4d712d839b4c3
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2021-08-19 15:42:10 +0200

    authentication is done

--- backend/solutions/06-07-validation-sanitization-relations/controllers/usersController.js
+++ backend/solutions/06-07-validation-sanitization-relations/controllers/usersController.js
@@ -2,6 +2,9 @@ const User = require("../models/User");
 const createError = require("http-errors");
 const { validationResult } = require("express-validator");
 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 exports.getUsers = async (req, res, next) => {
   try {
     const users = await User.find();
@@ -44,6 +47,8 @@ exports.updateUser = async (req, res, next) => {
 };
 
 exports.addUser = async (req, res, next) => {
+  const { firstName, lastName, email, password, address } = req.body;
+
   try {
     //Create array of error messages using validation result
     const errors = validationResult(req);
@@ -51,10 +56,113 @@ exports.addUser = async (req, res, next) => {
       res.status(422).json({ errors: errors.array() });
     }
 
-    const user = new User(req.body);
-    await user.save();
-    res.status(200).send(user);
+    //Check if user already exists
+    let existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ msg: "User Already Exists" });
+    }
+
+    //Create a new user
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password, // 1234
+      address,
+    });
+
+    //hash the password for the new user in order to save to DB
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(password, salt);
+
+    await newUser.save();
+
+    const payload = {
+      newUser: {
+        id: newUser.id,
+      },
+    };
+
+    console.log(payload);
+
+    jwt.sign(
+      payload,
+      "randomString",
+      {
+        expiresIn: "1h",
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.status(200).json({
+          token,
+        });
+      }
+    );
+
+    //res.status(200).send(user);
   } catch (e) {
     next(e);
+  }
+};
+
+exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({
+      email,
+    });
+    if (!user)
+      return res.status(400).json({
+        message: "User Not Exist",
+      });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({
+        message: "Incorrect Password !",
+      });
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      "randomString",
+      {
+        expiresIn: "1h",
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.status(200).json({
+          token,
+        });
+      }
+    );
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.userAuth = async (req, res) => {
+  try {
+    // request.user is getting fetched from Middleware after token authentication
+    const user = await User.findById(req.user.id);
+    res.json(user);
+  } catch (e) {
+    res.send({ message: "Error in Fetching user" });
   }
 };
--- backend/solutions/06-07-validation-sanitization-relations/routes/users.js
+++ backend/solutions/06-07-validation-sanitization-relations/routes/users.js
@@ -4,18 +4,26 @@ const router = express.Router();
 const { validateInputs } = require("../middleware/validator");
 const { userValidationRules } = require("../lib/validation/userRules");
 
+const auth = require("../middleware/auth");
+
 const {
   getUsers,
   getUser,
   updateUser,
   deleteUser,
   addUser,
+  loginUser,
+  userAuth,
 } = require("../controllers/usersController");
 
 router
   .route("/")
   .get(getUsers)
   .post(validateInputs(userValidationRules), addUser);
+
+router.route("/login").post(loginUser);
+
+router.get("/me", auth, userAuth);
 
 router.route("/:id").get(getUser).delete(deleteUser).put(updateUser);
 
